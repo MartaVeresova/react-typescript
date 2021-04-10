902958fd0e907390a0958a78c26fa4acb3a95ac1
Marta <martaveresovaa@gmail.com>
2021-04-10 22:58:10 +0300

    21.React (page of Dialogues - css)

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -5,7 +5,28 @@ import s from './Dialogues.module.css'
 export function Dialogues() {
     return (
         <div className={s.dialogues}>
-            Dialogues
+            <div className={s.dialoguesItem}>
+                <div className={s.item}>
+                    Marta
+                </div>
+                <div className={s.item}>
+                    Sasha
+                </div>
+                <div className={`${s.item} ${s.active}`}>
+                    Vera
+                </div>
+                <div className={s.item}>
+                    Anton
+                </div>
+                <div className={s.item}>
+                    Vanya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
