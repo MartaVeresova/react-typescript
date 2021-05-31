123c1e14eb9eca1a2ba7f6364c9143e749126aac
MartaVeresova <MartaVeresovaa@gmail.com>
2021-05-31 19:03:49 +0300

    53.React (class component; extends React.Component, typing; add '/users' to NavLink)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,59 +1,29 @@
 import React from 'react';
-import {UsersPropsType} from './UsersContainer';
-import s from './Users.module.css'
+import userPhoto from '../../assets/images/user.png';
+import s from './Users.module.css';
 import axios from 'axios';
-import userPhoto from '../../assets/images/user.png'
+import {UsersPropsType} from './UsersContainer';
 
-export function Users(props: UsersPropsType) {
-    const getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    props.setUsers(response.data.items)
+export class Users extends React.Component<UsersPropsType> {
 
-                    /*props.setUsers(
-                        [
-                            {
-                                id: v1(),
-                                followed: false,
-                                fullName: 'Marta',
-                                status: 'I am a boss',
-                                location: {city: 'Minsk', country: 'Belarus'},
-                                photoUrl: 'https://i.pinimg.com/564x/30/09/6b/30096bd46067ef042d16e5a67fff796b.jpg'
-                            },
-                            {
-                                id: v1(),
-                                followed: true,
-                                fullName: 'Katya',
-                                status: 'I am a boss too',
-                                location: {city: 'Moscow', country: 'Russia'},
-                                photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1583391/pub_5d6cd445fc69ab00aeeb8ab1_5d6cd44ffebcd400af21fde3/scale_1200'
-                            },
-                            {
-                                id: v1(),
-                                followed: false,
-                                fullName: 'Oleg',
-                                status: 'I am a boss too',
-                                location: {city: 'Kiev', country: 'Ukraine'},
-                                photoUrl: 'https://proprikol.ru/wp-content/uploads/2020/02/kartinki-na-avatarku-dlya-parnej-i-muzhchin-1-1.jpg'
-                            },
-                        ] as Array<UsersType>
-                    )*/
-                })
-        }
+    constructor(props: UsersPropsType) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
     }
 
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => {
+                            const onClickFollow = () => this.props.follow(u.id)
+                            const onClickUnFollow = () => this.props.unFollow(u.id)
 
-    return (
-        <div>
-            <button onClick={getUsers}>get users</button>
-            {
-                props.users.map(u => {
-                        const onClickFollow = () => props.follow(u.id)
-                        const onClickUnFollow = () => props.unFollow(u.id)
-
-                        return (
-                            <div key={u.id}>
+                            return (
+                                <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={s.userPhoto}/>
@@ -64,7 +34,7 @@ export function Users(props: UsersPropsType) {
                                 : <button onClick={onClickFollow}>follow</button>}
                         </div>
                     </span>
-                                <span>
+                                    <span>
                         <div>
                             {u.name}
                         </div>
@@ -72,15 +42,16 @@ export function Users(props: UsersPropsType) {
                             {u.status}
                         </div>
                         </span>
-                                <span>
+                                    <span>
                             <div>{'u.location.country'}</div>
                             <div>{'u.location.city'}</div>
                     </span>
-                            </div>
-                        )
-                    }
-                )
-            }
-        </div>
-    )
+                                </div>
+                            )
+                        }
+                    )
+                }
+            </div>
+        )
+    }
 }
