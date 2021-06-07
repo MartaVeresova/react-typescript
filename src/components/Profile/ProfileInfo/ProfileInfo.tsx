1c92e780cb39b667c2d888d7c5e3fb19c64756e1
MartaVeresova <MartaVeresovaa@gmail.com>
2021-06-07 15:37:24 +0300

    ProfileInfo <img />

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -14,7 +14,7 @@ export function ProfileInfo(props: PropsType) {
         <div className={s.profileInfo}>
             <div>
                 <img
-                    src=''/>
+                    src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
             </div>
             <div className={s.descriptionBlock}>
                 <img
