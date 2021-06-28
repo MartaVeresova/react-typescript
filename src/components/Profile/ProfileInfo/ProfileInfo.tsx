08d8b829398ed5efb7c2795d104abb4c81c0cae8
MartaVeresova <MartaVeresovaa@gmail.com>
2021-06-28 22:40:51 +0300

    71.React (setState, local state, ProfileStatus)

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import s from './ProfileInfo.module.css'
 import {ProfileType} from '../../../redux/profile-reducer';
 import {Preloader} from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus';
 
-type PropsType= {
+type PropsType = {
     profile: ProfileType | null
 }
+
 export function ProfileInfo(props: PropsType) {
     if (!props.profile) {
         return <Preloader/>
@@ -14,12 +16,13 @@ export function ProfileInfo(props: PropsType) {
         <div className={s.profileInfo}>
             <div>
                 <img
-                    src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' alt={''}/>
+                    src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+                    alt={''}/>
             </div>
             <div className={s.descriptionBlock}>
                 <img
                     src={props.profile.photos.large} alt={''}/>
-                ava+description
+                <ProfileStatus status={'Hello'}/>
             </div>
         </div>
     )
