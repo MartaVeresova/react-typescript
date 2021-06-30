488d3ab652723bd6310da1476a11fe7f7c04e8fe
MartaVeresova <MartaVeresovaa@gmail.com>
2021-06-30 17:20:54 +0300

    75.React (redux-form; login)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
+
 
 export const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
     return (
         <div>
-            LOGIN
+            <h1>LOGIN</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
+
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    console.log('rerender')
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'}/>
+            </div>
+            <div>
+                <Field type="checkbox" name={'rememberMe'} component={'input'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,6 +5,7 @@ import dialogsReducer from './dialogs-reducer';
 import sidebarReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ const rootReducer = combineReducers({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
