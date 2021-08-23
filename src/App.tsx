02b2706d994c215652e3b1ccd7166274680da382
MartaVeresova <MartaVeresovaa@gmail.com>
2021-08-23 15:18:26 +0300

    92.React (react-test-renderer)

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import {Navbar} from './components/Navbar/Navbar'
 import {News} from './components/News/News';
 import {Music} from './components/Music/Music';
 import {Settings} from './components/Settings/Settings';
-import {Route, withRouter} from 'react-router-dom'
+import {BrowserRouter, Route, withRouter} from 'react-router-dom'
 import {DialogsContainer} from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import {connect} from 'react-redux';
+import {connect, Provider} from 'react-redux';
 import {compose} from 'redux';
 import {initializeApp} from './redux/app-reducer';
-import {AppStateType} from './redux/store';
+import {AppStateType, store} from './redux/store';
 import {Preloader} from './components/common/Preloader/Preloader';
 
 
@@ -59,8 +59,17 @@ type MapDispatchToPropsType = {
 export type AppPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 
-export default compose<ComponentType>(
+const AppContainer = compose<ComponentType>(
     withRouter,
     connect(mapStateToProps, {initializeApp}))(App)
 
 
+export const SamuraiJSApp = () => {
+   return <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppContainer/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
-import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux';
-import {store} from './redux/store';
-import App from './App';
+import {SamuraiJSApp} from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
+ReactDOM.render(<SamuraiJSApp />,
     document.getElementById('root')
 )
 
